026688e2afde62e8e9a0ca40f18e3471c5572621
wika12l <91103104+wika12l@users.noreply.github.com>
2021-10-10 12:48:59 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var build=document.getElementById("builder");
+function btninfo(){
+    build.style.display='block';
+}
+function btnclose(){
+    build.style.display='none';
+}
+var allidmusic=document.getElementById("music");
+function search(){
+    var txtSearch=document.getElementById("txtsearch").value;
+    if(txtSearch=="music")
+    {
+       alert.getElementById.style.display
+    }
+}
